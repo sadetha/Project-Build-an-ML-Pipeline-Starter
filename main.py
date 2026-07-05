e98db7ac2707ee7f076021f61864ad25a59454b1
sadetha <sarahjgnass@gmail.com>
2026-07-05 18:30:27 +0000

    completed model selection and test, updated config with new values

--- main.py
+++ main.py
@@ -112,12 +112,13 @@ def go(config: DictConfig):
             )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            mlflow.run(
+            f"{config['main']['components_repository']}/test_regression_model",'main', 
+            parameters = {
+                'mlflow_model': 'random_forest_export:prod',
+                'test_dataset': 'test_data.csv:latest'
+                }
+            )
 
 
 if __name__ == "__main__":
